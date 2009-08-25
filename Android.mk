026630215f76b4d798e3f6e24018ec853e2e0d51
Andrei Dolnikov <adolnikov@embeddedalley.com>
2009-08-25 08:55:11 -0400

    Build libmad only if BUILD_WITH_GST option provided.
    Signed-off-by: Andrei Dolnikov <adolnikov@embeddedalley.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(strip $(BUILD_WITH_GST)),true)
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -27,3 +29,5 @@ LOCAL_CFLAGS := \
     -DFPM_DEFAULT
 
 include $(BUILD_STATIC_LIBRARY)
+
+endif
